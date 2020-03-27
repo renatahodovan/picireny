321ecbe56709ca332417d707b797b27d284acb0b
Akos Kiss <akiss@inf.u-szeged.hu>
2020-03-28 00:46:58 +0100

    Speed up testing by reducing the number of parameter combinations
    Line coverage is kept unchanged.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2018 Renata Hodovan, Akos Kiss.
+# Copyright (c) 2016-2020 Renata Hodovan, Akos Kiss.
 #
 # Licensed under the BSD 3-Clause License
 # <LICENSE.rst or https://opensource.org/licenses/BSD-3-Clause>.
@@ -27,73 +27,32 @@ antlr = os.getenv('ANTLR')
     ('test-json-obj-arr-87', 'inp-obj-arr.json', 'exp-obj-arr-87.json', 'JSON.g4', 'json', None),
     ('test-inijson-str-arr-87', 'inp-str-arr.ini', 'exp-str-arr-87.ini', None, None, 'inijson.json'),
 ])
-class TestCli:
-
-    def _run_picireny(self, test, inp, exp, grammar, rule, input_format, tmpdir, args):
-        out_dir = '%s' % tmpdir
-        cmd = (sys.executable, '-m', 'picireny') \
-              + ('--test=' + test + script_ext, '--input=' + inp, '--out=' + out_dir) \
-              + ('--log-level=DEBUG', )
-        if grammar:
-            cmd += ('--grammar=' + grammar, )
-        if rule:
-            cmd += ('--start=' + rule, )
-        if input_format:
-            cmd += ('--format=' + input_format, )
-        if antlr:
-            cmd += ('--antlr=' + antlr, )
-        cmd += args
-        proc = subprocess.Popen(cmd, cwd=resources_dir)
-        proc.communicate()
-        assert proc.returncode == 0
-        assert filecmp.cmp(os.path.join(out_dir, inp), os.path.join(resources_dir, exp))
-
-    @pytest.mark.parametrize('args_parallel', [
-        (),
-    ])
-    @pytest.mark.parametrize('args_cache', [
-        ('--cache=none', ),
-        ('--cache=config', ),
-        ('--cache=content', ),
-    ])
-    @pytest.mark.parametrize('args_parser', [
-        (),
-        ('--parser=java', ),
-    ])
-    @pytest.mark.parametrize('args_hdd_star', [
-        (),
-        ('--no-hdd-star', ),
-    ])
-    @pytest.mark.parametrize('args_squeeze', [
-        (),
-        ('--no-squeeze-tree', ),
-    ])
-    @pytest.mark.parametrize('args_skip_unremovable', [
-        (),
-        ('--no-skip-unremovable', ),
-    ])
-    def test_light(self, test, inp, exp, grammar, rule, input_format, tmpdir, args_parallel, args_cache, args_parser, args_hdd_star, args_squeeze, args_skip_unremovable):
-        self._run_picireny(test, inp, exp, grammar, rule, input_format, tmpdir,
-                           args_parallel + args_cache + args_parser + args_hdd_star + args_squeeze + args_skip_unremovable)
-
-    @pytest.mark.parametrize('args_parallel', [
-        ('--parallel', ),
-    ])
-    @pytest.mark.parametrize('args_cache', [
-        (),
-    ])
-    @pytest.mark.parametrize('args_parser', [
-        (),
-    ])
-    @pytest.mark.parametrize('args_hdd_star', [
-        (),
-    ])
-    @pytest.mark.parametrize('args_squeeze', [
-        (),
-    ])
-    @pytest.mark.parametrize('args_skip_unremovable', [
-        (),
-    ])
-    def test_parallel(self, test, inp, exp, grammar, rule, input_format, tmpdir, args_parallel, args_cache, args_parser, args_hdd_star, args_squeeze, args_skip_unremovable):
-        self._run_picireny(test, inp, exp, grammar, rule, input_format, tmpdir,
-                           args_parallel + args_cache + args_parser + args_hdd_star + args_squeeze + args_skip_unremovable)
+@pytest.mark.parametrize('args', [
+    ('--cache=config', ),
+    ('--no-skip-unremovable', '--parser=java', '--cache=content', ),
+    ('--no-squeeze-tree', '--parser=java', '--cache=none', ),
+    ('--no-squeeze-tree', '--no-skip-unremovable', '--cache=config', ),
+    ('--no-hdd-star', '--parser=java', '--cache=content', ),
+    ('--no-hdd-star', '--no-skip-unremovable', '--cache=none', ),
+    ('--no-hdd-star', '--no-squeeze-tree', '--cache=config', ),
+    ('--no-hdd-star', '--no-squeeze-tree', '--no-skip-unremovable', '--parser=java', '--cache=content', ),
+    ('--parallel', ),
+])
+def test_cli(test, inp, exp, grammar, rule, input_format, args, tmpdir):
+    out_dir = '%s' % tmpdir
+    cmd = (sys.executable, '-m', 'picireny') \
+          + ('--test=' + test + script_ext, '--input=' + inp, '--out=' + out_dir) \
+          + ('--log-level=DEBUG', )
+    if grammar:
+        cmd += ('--grammar=' + grammar, )
+    if rule:
+        cmd += ('--start=' + rule, )
+    if input_format:
+        cmd += ('--format=' + input_format, )
+    if antlr:
+        cmd += ('--antlr=' + antlr, )
+    cmd += args
+    proc = subprocess.Popen(cmd, cwd=resources_dir)
+    proc.communicate()
+    assert proc.returncode == 0
+    assert filecmp.cmp(os.path.join(out_dir, inp), os.path.join(resources_dir, exp))
